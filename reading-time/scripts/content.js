a803d1b143d92e7f4ba4849042bb9cbf94545d84
Putra Prima Arhandi <putraprima@gmail.com>
2023-08-16 18:33:55 +0700

    update reading time goal.com

--- reading-time/scripts/content.js
+++ reading-time/scripts/content.js
@@ -1,1 +1,23 @@
 const article = document.querySelector("article");
+console.log("Loaded content script");
+// `document.querySelector` may return null if the selector doesn't match anything.
+if (article) {
+  const text = article.textContent;
+  console.log(text);
+  const wordMatchRegExp = /[^\s]+/g; // Regular expression
+  const words = text.matchAll(wordMatchRegExp);
+  // matchAll returns an iterator, convert to array to get word count
+  const wordCount = [...words].length;
+  const readingTime = Math.round(wordCount / 200);
+  const badge = document.createElement("p");
+  // Use the same styling as the publish information in an article's header
+  badge.classList.add("color-secondary-text", "type--caption");
+  badge.textContent = `⏱️ ${readingTime} min read ⏰`;
+
+  // Support for API reference docs
+  const heading = article.querySelector("h1");
+  // Support for article docs with date
+  const date = article.querySelector("time")?.parentNode;
+
+  (date ?? heading).insertAdjacentElement("afterend", badge);
+}
